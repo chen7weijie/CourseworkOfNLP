ec314e3601c88ee6f78227d671f9cdf3b57a3f5c
chen7weijie <chen7weijie@163.com>
2019-04-23 15:51:08 +0800

    sample of tokennizaiton

---
+++ Tokenization.py
@@ -0,0 +1,4 @@
+from nltk.tokenize import sent_tokenize
+myText="is upset that he can't update his Facebook by texting it... and might cry as a result  School today also. Blah!"
+#切分句子
+print(sent_tokenize(myText))
